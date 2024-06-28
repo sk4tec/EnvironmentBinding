8ed793c90a1e797aa964c8f2536f417e55dd3bda
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-28 20:26:15 +0100

    Initial Commit

---
+++ EnvironmentBinding/EnvironmentBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentBindingApp.swift
+//  EnvironmentBinding
+//
+//  Created by Sunjay Kalsi on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
