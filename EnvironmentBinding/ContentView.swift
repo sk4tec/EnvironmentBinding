d1bebae887669bb494716ec5164897b07f2b34c0
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-28 20:52:49 +0100

    Add Master Detail Environment example
    Note where .environmentObject(data) is put this code can break

--- EnvironmentBinding/ContentView.swift
+++ EnvironmentBinding/ContentView.swift
@@ -1,21 +1,41 @@
-//
-//  ContentView.swift
-//  EnvironmentBinding
-//
-//  Created by Sunjay Kalsi on 28/06/2024.
-//
-
 import SwiftUI
 
+class Data: ObservableObject {
+    @Published public var value: Bool = false
+
+    public init(value: Bool = false) {
+        self.value = value
+    }
+}
+
 struct ContentView: View {
+    @StateObject private var data = Data()
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Value in ContentView \(data.value)")
+                NavigationLink("Go To View", destination: DetailView())
+        //      NavigationLink("Go To View", destination: DetailView().environmentObject(data)) // Good
+            }
+         // .environmentObject(data) // Not Good
+            .padding()
+        }
+        .environmentObject(data) // Good
+    }
+}
+
+struct DetailView: View {
+    @EnvironmentObject var sharedVar: Data
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Detail View")
+            Text("Value of data is \(sharedVar.value)")
+
+            Button("Toggle data") {
+                sharedVar.value.toggle()
+            }
         }
-        .padding()
     }
 }
 
